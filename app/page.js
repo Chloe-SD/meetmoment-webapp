447877508fb4080710d60e250de7e2edbd9365fe
Chloe-SD <chloe.nibali@edu.sait.ca>
2024-07-09 19:26:33 -0600

    initial commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        Welcome to Chloe's final project webapp for CPRG-306<br/><br/>
+        This will be the web interface of the MeetMoment app! A multi platform, intuitive, and efficient meeting scheduling application!
+      </div>
+    </main>
+  );
+}
