a19474b14a758201b6b8624deb0246dd483bc771
Chloe-SD <chloe.nibali@edu.sait.ca>
2024-07-09 19:30:54 -0600

    bug fix

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <div>
-        Welcome to Chloe's final project webapp for CPRG-306<br/><br/>
+        Welcome to Chloe&apos;s final project webapp for CPRG-306<br/><br/>
         This will be the web interface of the MeetMoment app! A multi platform, intuitive, and efficient meeting scheduling application!
       </div>
     </main>
