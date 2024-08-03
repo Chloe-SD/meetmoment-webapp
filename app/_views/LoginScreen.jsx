fbcce6e761baaced0c8ae3da46c5946a27a319d8
Chloe-SD <chloe.nibali@edu.sait.ca>
2024-08-03 12:25:24 -0600

    try Image reather than img

--- app/_views/LoginScreen.jsx
+++ app/_views/LoginScreen.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { useUserAuth } from "../_utils/auth-context";
 import LoginForm from "../components/LoginForm";
+import Image from "next/image";
 
 
 const LoginScreen = () => {
@@ -19,7 +20,7 @@ const LoginScreen = () => {
             <div className="flex space-x-2 bg-blue-500 px-2 pt-6 rounded-md
             border-2 border-purple-50 shadow-sm shadow-purple-200 m-4">
                 <div className="flex-1">
-                    <img src="/meeting1.jpg" alt="meeting" />
+                    <Image src="/meeting1.jpg" alt="meeting" width={500} height={300}/>
                     <a title="photo by fauxels - View on Pexels" target="_blank"
                     href="https://www.pexels.com/photo/positive-diverse-coworkers-chatting-and-sitting-at-table-with-laptop-4350093/">
                         <p className="text-gray-400 hover:text-gray-300">photo by Ketut Subiyanto</p>
@@ -27,7 +28,7 @@ const LoginScreen = () => {
                 </div>
 
                 <div className="flex-1">
-                    <img src="/meeting3.jpg" alt="meeting" />
+                    <Image src="/meeting3.jpg" alt="meeting" width={500} height={300}/>
                     <a title="photo by fauxels - View on Pexels" target="_blank"
                     href="https://www.pexels.com/photo/group-of-friends-hanging-out-933964/">
                         <p className="text-gray-400 hover:text-gray-300">photo by Helena Lopes</p>
@@ -35,7 +36,7 @@ const LoginScreen = () => {
                 </div>
 
                 <div className="flex-1">
-                    <img src="/meeting2.jpg" alt="meeting" />
+                    <Image src="/meeting2.jpg" alt="meeting" width={500} height={300}/>
                     <a title="photo by fauxels - View on Pexels" target="_blank"
                     href="https://www.pexels.com/photo/group-of-people-gathered-around-wooden-table-3184360/">
                         <p className="text-gray-400 hover:text-gray-300">Photo by fauxels</p>
@@ -43,7 +44,7 @@ const LoginScreen = () => {
                 </div>
 
                 <div className="flex-1">
-                    <img src="/meeting4.jpg" alt="meeting" />
+                    <Image src="/meeting4.jpg" alt="meeting" width={500} height={300}/>
                     <a title="photo by fauxels - View on Pexels" target="_blank"
                     href="https://www.pexels.com/photo/group-of-people-having-a-meeting-on-a-restaurant-9543814/">
                         <p className="text-gray-400 hover:text-gray-300">Photo by Mikhail Nilov</p>
